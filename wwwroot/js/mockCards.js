c9183f9bfd490387fbd820f77861f6d04cc9cc04
Deniz Gökbaş <dng.develops@gmail.com>
2022-08-17 20:30:46 +0300

    More Dynamic mock cards

--- wwwroot/js/mockCards.js
+++ wwwroot/js/mockCards.js
@@ -6,10 +6,58 @@ const cardExpMonth = document.getElementById("cardExpMonth");
 const cardExpYear = document.getElementById("cardExpYear");
 const carCvc = document.getElementById("carCvc");
 
+const mockList = [
+    {
+        cName : "John Doe",
+        cNumber: "5528790000000008",
+        cExpMonth : "12",
+        cdExpYear : "2030",
+        cCvc : "123",
+
+    },
+    {
+        cName: "ObiWan Kenobi",
+        cNumber: "5890040000000016",
+        cExpMonth: "12",
+        cdExpYear: "2030",
+        cCvc: "123",
+
+    },
+    {
+        cName: "Richard Dawkins",
+        cNumber: "5526080000000006",
+        cExpMonth: "12",
+        cdExpYear: "2030",
+        cCvc: "123",
+
+    },
+    {
+        cName: "Morgan Freeman",
+        cNumber: "4766620000000001",
+        cExpMonth: "12",
+        cdExpYear: "2030",
+        cCvc: "123",
+
+    },
+    {
+        cName: "Joe Citizen",
+        cNumber: "4603450000000000",
+        cExpMonth: "12",
+        cdExpYear: "2030",
+        cCvc: "123",
+
+    }
+]
+
+
+
+
 el.addEventListener("click", () => {
-    cardName.value = "John Doe"
-    cardNumber.value = "5528790000000008"
-    cardExpMonth.value = "12"
-    cardExpYear.value = "2030"
-    carCvc.value = "123"
+    let randNum = Math.floor(Math.random() * mockList.length);
+    cardName.value = mockList[randNum].cName
+    cardNumber.value = mockList[randNum].cNumber
+    cardExpMonth.value = mockList[randNum].cExpMonth
+    cardExpYear.value = mockList[randNum].cdExpYear
+    carCvc.value = mockList[randNum].cCvc
+   
 })
